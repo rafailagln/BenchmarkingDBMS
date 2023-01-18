407767ae05242aa97d7b83e983e5c03d375884fd
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-01-18 17:59:54 +0200

    Changes to types of UDFs and at queries

--- MonetDB/311/queries/311MonetDBscript.sql
+++ MonetDB/311/queries/311MonetDBscript.sql
@@ -1,18 +1,18 @@
-CREATE OR REPLACE FUNCTION fix_zip_codes(x STRING) RETURNS STRING LANGUAGE PYTHON {
+CREATE OR REPLACE FUNCTION fix_zip_codes(x varchar(20)) RETURNS varchar(20) LANGUAGE PYTHON {
     import string
     array = []
     for zips in x:
         if not zips:
-            array.append(None)
+            array.append("None")
 
         s = zips[:5]
 
         if s == "00000":
-            array.append(None)
+            array.append("None")
         else:
             array.append(s)
     return array
 };
 
 
-SELECT DISTINCT sys.fix_zip_codes("Incident Zip") AS Incident_Zip FROM sys."311";
+SELECT DISTINCT fix_zip_codes("Incident_Zip") AS Incident_Zip FROM "311";
