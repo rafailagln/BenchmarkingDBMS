372b2544d4ae85daa2109d43622cf3df41deabc9
Notaris <80954329+notarisj@users.noreply.github.com>
2023-01-20 02:32:04 +0200

    Changed varchar(80) -> varchar(500) in dataMultiplicationWeblogs because it was truncated

--- Experiments/DataMutliplication/Vertica/dataMultiplicationWeblogs.sql
+++ Experiments/DataMutliplication/Vertica/dataMultiplicationWeblogs.sql
@@ -1,33 +1,33 @@
 CREATE TABLE "weblogs_2" (
-    "c1" varchar
+    "c1" varchar(500)
 );
 
 INSERT INTO "weblogs_2" ("c1") SELECT "c1" FROM "weblogs";
 INSERT INTO "weblogs_2" ("c1") SELECT "c1" FROM "weblogs";
 
 CREATE TABLE "weblogs_4" (
-    "c1" varchar
+    "c1" varchar(500)
 );
 
 INSERT INTO "weblogs_4" ("c1") SELECT "c1" FROM "weblogs_2";
 INSERT INTO "weblogs_4" ("c1") SELECT "c1" FROM "weblogs_2";
 
 CREATE TABLE "weblogs_8" (
-    "c1" varchar
+    "c1" varchar(500)
 );
 
 INSERT INTO "weblogs_8" ("c1") SELECT "c1" FROM "weblogs_4";
 INSERT INTO "weblogs_8" ("c1") SELECT "c1" FROM "weblogs_4";
 
 CREATE TABLE "weblogs_16" (
-    "c1" varchar
+    "c1" varchar(500)
 );
 
 INSERT INTO "weblogs_16" ("c1") SELECT "c1" FROM "weblogs_8";
 INSERT INTO "weblogs_16" ("c1") SELECT "c1" FROM "weblogs_8";
 
 CREATE TABLE "weblogs_32" (
-    "c1" varchar
+    "c1" varchar(500)
 );
 
 INSERT INTO "weblogs_32" ("c1") SELECT "c1" FROM "weblogs_16";
